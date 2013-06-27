9a0fafa135ccb5308c50798c2434934f9e4fdd52
Kyle Phillips <kyle.phillips@b-reel.com>
2013-06-26 18:14:44 -0700

    fleshing out api with callbacks and separation between loading data and loading imagery

--- src/GSVPano.js
+++ src/GSVPano.js
@@ -6,13 +6,13 @@ GSVPANO.PanoLoader = function (parameters) {
 	var _parameters = parameters || {},
 		_location,
 		_zoom,
-		_panoId,
 		_panoClient = new google.maps.StreetViewService(),
 		_count = 0,
 		_total = 0,
 		_canvas = document.createElement('canvas'),
 		_ctx = _canvas.getContext('2d'),
 		rotation = 0,
+        composeFromTile,
         self = this;
 
     //override to receive progress events during load
@@ -23,6 +23,8 @@ GSVPANO.PanoLoader = function (parameters) {
     };
     //ovverride to get notified when the panorama has loaded
     this.onPanoramaLoad = function(){};
+    //override to get the panorama data
+    this.onPanoramaData = function(){};
     //override to get events when panorama failed to load data
     this.onNoPanoramaData = function(status){};
 
@@ -37,25 +39,14 @@ GSVPANO.PanoLoader = function (parameters) {
 		_ctx.scale(-1, 1);
 	};
 
-    //place a single tile of the panorama into the stitched canvas
-	this.composeFromTile = function (x, y, texture) {
-		_ctx.drawImage(texture, x * 512, y * 512);
-		_count++;
-		
-		var p = Math.round(_count * 100 / _total);
-		self.onProgress(p);
-		
-		if (_count === _total) {
-			this.canvas = _canvas;
-			this.onPanoramaLoad();
-		}
-		
-	};
 
-    //load the individual tiles and paint to their corresponding location
-    //on the canvas
-	this.composePanorama = function () {
-	
+    /**
+     * load the individual tiles and paint to their corresponding location on the canvas
+     * @param {String} panoId the id to the panoramic image, provided by google (use #find())
+     * @param {Function} callback the callback for when the canvas is complete, invoked with signature `( error, callback )`
+     */
+	this.composePanorama = function ( panoId, callback ) {
+	    callback = callback || function(){};
 		this.onProgress(0);
 		console.log('Loading panorama for zoom ' + _zoom + '...');
 		
@@ -64,17 +55,27 @@ GSVPANO.PanoLoader = function (parameters) {
 			url,
 			x,
 			y;
-			
+
 		_count = 0;
-		_total = w * h;
+		_total = (w) * h;
+
 		
 		for( y = 0; y < h; y++) {
 			for( x = 0; x < w; x++) {
-				url = 'http://maps.google.com/cbk?output=tile&panoid=' + _panoId + '&zoom=' + _zoom + '&x=' + x + '&y=' + y + '&' + Date.now();
+				url = 'http://maps.google.com/cbk?output=tile&panoid=' + panoId + '&zoom=' + _zoom + '&x=' + x + '&y=' + y + '&' + Date.now();
+                console.log(url);
 				(function (x, y) { 
 					var img = new Image();
 					img.addEventListener('load', function () {
-						self.composeFromTile(x, y, this);
+                        _ctx.drawImage( this, x*512, y*512 );
+                        _count++;
+                        var p = Math.round(_count * 100 / _total);
+                        self.onProgress(p);
+                        if (_count === _total) {
+                            self.canvas = _canvas;
+                            self.onPanoramaLoad();
+                            callback( null, canvas );
+                        }	
 					});
 					img.crossOrigin = '';
 					img.src = url;
@@ -87,28 +88,48 @@ GSVPANO.PanoLoader = function (parameters) {
     /**
      * load the panorama for the provided location
      * @param {google.maps.LatLng} location the geographic location to load
-     * @param {Number} [radius] radius to search for closest (in meters), defaults to 50 meters
+     * @param {Function} callback receives param signature `( err, canvas )` with `this` in scope of the PanoLoader instance
      */
-	this.load = function (location, radius) {
-        radius = radius || 50;
+	this.load = function ( location, callback ) {
 		console.log('Load for', location);
+        self.find( location, function( err, pano ){
+            if(err){
+                return;
+            }
+            self.composePanorama( pano.id, callback );
+        });
+	};
+
+    /**
+     * make a request to find panoramic meta-data from google
+     * @param {google.maps.LatLng} location
+     * @param {Function} callback invoked with param signature `( error, panoData )`
+     */
+    this.find = function( location, callback ){
+        var radius = 50,
+            err = null,
+            resp = {};
+
 		_panoClient.getPanoramaByLocation(location, radius, function (result, status) {
 			if (status === google.maps.StreetViewStatus.OK) {
 				self.onPanoramaData( result );
 				var h = google.maps.geometry.spherical.computeHeading(location, result.location.latLng);
 				rotation = (result.tiles.centerHeading - h) * Math.PI / 180.0;
                 //expose the copyright info on the instance
-				self.copyright = result.copyright;
-				_panoId = result.location.pano;
-				self.location = location;
-				self.composePanorama();
-			} else {
+				resp.copyright = result.copyright;
+                resp.pitch = result.tiles.originPitch;
+                resp.heading = result.tiles.centerHeading;
+				resp.location = location;
+                resp.imageDate = result.imageDate;
+                resp.id = result.location.pano;
+            } else {
 				self.onNoPanoramaData( status );
-			    self.onError('Could not retrieve panorama for the following reason: ' + status);
+                err = Error('Could not retrieve panorama for the following reason: ' + status);
+			    self.onError(err.message);
 			}
+            callback( err, resp );
 		});
-		
-	};
+    };
 	
     /**
      * set the zoom level for the panorama
